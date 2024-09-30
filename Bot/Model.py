54bf4f7d4aea482c655d265027e31fd5152e3b21
SamVOja <164084228+SamVOja@users.noreply.github.com>
2024-09-30 23:41:40 +0300

    Update Model.py

--- Bot/Model.py
+++ Bot/Model.py
@@ -10,16 +10,18 @@ class Model(torch.nn.Module):
         self.n_cards = n_cards
         self.n_archetypes = 10 
         self.relu = torch.nn.ReLU()
+        self.dropout = torch.nn.Dropout(p=0.3)
         self.linear = torch.nn.Linear(321, 10)
 
-        #self.linear.bias = torch.nn.Parameter(torch.tensor(1.0))
         self.linear.bias.data.fill_(0.5)
         
         if random_initial:
             self.weights = self.Random_Weights()
         else:
             self.weights = self.Initial_Weights()
-
+            
+        self.linear.weight = torch.nn.Parameter(self.weights.T) 
+        
     def Random_Weights(self): 
         """Weights initialized as a Uniform distribution"""
         initial_weights = torch.nn.Parameter(torch.rand(self.n_cards, self.n_archetypes))
@@ -52,12 +54,20 @@ class Model(torch.nn.Module):
         index = torch.arange(1, batch_size + 1, device=Avg_Cost.device).float()
         Avg_Cost /= index #avg mana of pool at every pick
 
-        self.linear.weight = torch.nn.Parameter(self.weights.T)
         arch_bias = self.relu(self.linear(pool))
+        
         card_weights = (cards.view((batch_size, self.n_cards, 1)) * self.weights.reshape((1, self.n_cards, self.n_archetypes)))
         #shape[39, 321, 10]
         
         arch_bias_expanded = arch_bias.unsqueeze(1) # Shape [39, 1, 10]
 
         current_choice = torch.mul(arch_bias_expanded, card_weights).sum(dim=-1)
-        return F.softmax(current_choice, dim=-1)
+
+        return F.relu(current_choice)
+    
+    def stable_non_zero_log_softmax(self, x):
+        b = x.max(dim=1).values.view(-1, 1)
+        stabalized_x = (x - b * x.sign())
+        log_sum_exps = torch.log(torch.sum(x.sign() * torch.exp(stabalized_x), dim=1))
+        log_probs = x.sign() * (stabalized_x - log_sum_exps.view(-1, 1))
+        return log_probs
