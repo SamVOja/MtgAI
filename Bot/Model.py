5875858241af62d8d124c3f88de56823e86c46f3
SamVOja <164084228+SamVOja@users.noreply.github.com>
2024-10-12 18:46:12 +0300

    Update Model.py

--- Bot/Model.py
+++ Bot/Model.py
@@ -7,20 +7,22 @@ class Model(torch.nn.Module):
     def __init__(self, n_cards, random_initial):
         super().__init__() 
         self.colors = ["WU", "WB", "WR", "WG", "UB", "UR", "UG", "BR", "BG", "RG"]
-        self.n_cards = n_cards
+        self.n_cards = n_cards #321
         self.n_archetypes = 10 
+        self.n_neurons = 128
+        
         self.relu = torch.nn.ReLU()
         self.dropout = torch.nn.Dropout(p=0.3)
-        self.linear = torch.nn.Linear(321, 10)
 
-        self.linear.bias.data.fill_(0.5)
-        
+        self.hidden = torch.nn.Linear(self.n_cards, self.n_neurons)  
+        self.output = torch.nn.Linear(self.n_neurons, self.n_archetypes)  
+
         if random_initial:
             self.weights = self.Random_Weights()
         else:
             self.weights = self.Initial_Weights()
             
-        self.linear.weight = torch.nn.Parameter(self.weights.T) 
+        self.output.bias.data.fill_(0.5)
         
     def Random_Weights(self): 
         """Weights initialized as a Uniform distribution"""
@@ -54,7 +56,12 @@ class Model(torch.nn.Module):
         index = torch.arange(1, batch_size + 1, device=Avg_Cost.device).float()
         Avg_Cost /= index #avg mana of pool at every pick, unused
 
-        arch_bias = self.relu(self.linear(pool))
+        hidden_output = self.hidden(pool) 
+        hidden_output = self.relu(hidden_output)
+        hidden_output = self.dropout(hidden_output)
+        
+        arch_bias = self.output(hidden_output)
+        arch_bias = self.relu(arch_bias)
         
         card_weights = (cards.view((batch_size, self.n_cards, 1)) * self.weights.reshape((1, self.n_cards, self.n_archetypes)))
         #shape[39, 321, 10]
